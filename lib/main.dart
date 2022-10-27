8abc3440986b95bd84e5a732dfc8681ecd04fbbe
Dangbok <ghdwldnjs99@naver.com>
2022-10-27 18:07:27 +0900

    할 일 관리

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,160 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Firebase를 사용하기 위한 임포트
+import 'package:cloud_firestore/cloud_firestore.dart'; // DB에 저장된 할 일 목록 가져오기
 
-void main()=> runApp(MyApp());
+void main() async {
+  // Firebase와 연동
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Firebase 초기화
+  runApp(MyApp());
+}
 
+// 할 일 클래스
+class Todo {
+  bool isDone;
+  String title;
+
+  Todo(this.title, {this.isDone = false}); // 할 일 목록 가져오기
+}
+
+// 할 일 관리
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: '할 일 관리',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  // final _items =
+  //     <Todo>[]; // 할 일 목록을 저장할 리스트(리스트 작성시 <Todo>를 앞에 작성하여 Todo 객체를 담는 것을 명시)
+  var _todoController = TextEditingController(); // 할 일 문자열 조작을 위한 컨트롤러
+
+  @override
+  void dispose() {
+    _todoController.dispose(); // 사용이 끝나면 해제
+    super.dispose();
+  }
+
+  // 할 일 객체를 ListTile 형태로 변경하는 메서드
+  Widget _buildItemWidget(DocumentSnapshot doc) {
+    // Firestore 문서는 DocumentSnapshot 클래스의 인스턴스이기 때문에 이를 받아서 Todo 객체를 생성
+    final todo = Todo(doc['title'], isDone: doc['isDone']);
+
+    return ListTile(
+      onTap: () => _toggleTodo(doc), // Todo: 클릭 시 완료/미완료되도록 수정
+      title: Text(
+        todo.title, // 할 일
+        style: todo.isDone // 완료일 때는 스타일 적용
+            ? TextStyle(
+                decoration: TextDecoration.lineThrough, // 취소선
+                fontStyle: FontStyle.italic, // 이탤릭체
+              )
+            : null, // 아무 스타일도 적용 안함
+      ),
+      trailing: IconButton(
+        icon: Icon(Icons.delete_forever),
+        onPressed: () => _deleteTodo(doc), // Todo: 쓰레기통 클릭시 삭제되도록 수정
+      ),
+    );
+  }
+
+  // 할 일 추가 메서드
+  void _addTodo(Todo todo) {
+    // 할 일 추가하기
+    FirebaseFirestore.instance.collection('todo').add({
+      'title': todo.title,
+      'isDone': todo.isDone
+    }); // add() 메서드에는 Map 형식으로 데이터를 작성함
+    _todoController.text = '';
+
+    // setState(() {
+    //   _items.add(todo);
+    //   _todoController.text=''; // 할 일 입력 필드를 비움
+    // });
+  }
+
+  // 할 일 삭제 메서드
+  void _deleteTodo(DocumentSnapshot doc) {
+    FirebaseFirestore.instance.collection('todo').doc(doc.id).delete();
+
+    // setState(() {
+    //   _items.remove(todo);
+    // });
+  }
+
+  // 할 일 완료/미완료 메서드
+  void _toggleTodo(DocumentSnapshot doc) {
+    // 특정 문서를 업데이트하려면 문서 ID가 필요한데 DocumentSnapshot을 통해 문서 ID를 얻을 수 있으며 doc() 메서드에 인수로 전달하고 update()메서드에 수정하고자 하는 내용을 Map형태로 전달하면 자료가 업데이트됨
+    FirebaseFirestore.instance.collection('todo').doc(doc.id).update({
+      'isDone': !doc['isDone'],
+    });
+
+    // setState(() {
+    //   todo.isDone = !todo.isDone;
+    // });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('남은 할 일'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _todoController,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () => _addTodo(Todo(_todoController.text)),
+                  // 기능과 UI 연결
+                  child: Text('추가'),
+                ),
+              ],
+            ),
+            StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance.collection('todo').snapshots(),
+                // todo 컬렉션에 있는 모든 문서를 스트림으로 얻음 스트림은 자료가 변경되었을 때 반응하여 화면을 다시 그려줌
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    // snapshot에는 데이터를 포함하여 다양한 정보가 들어있음
+                    return CircularProgressIndicator();
+                  }
+                  final documents = snapshot.data!.docs; // 모든 문서를 얻음
+                  return Expanded(
+                    child: ListView(
+                      children: documents
+                          .map((doc) => _buildItemWidget(doc))
+                          .toList(), // 전체 UI와 할 일 목록 UI 결합
+                    ),
+                  );
+                }),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main6.dart
+++ lib/main6.dart
@@ -1,130 +1,130 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-void main() => runApp(MyApp());
-
-// 스탑워치
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'StopWatch',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: StopWatchPage(),
-    );
-  }
-}
-
-class StopWatchPage extends StatefulWidget {
-  const StopWatchPage({Key? key}) : super(key: key);
-
-  @override
-  State<StopWatchPage> createState() => _StopWatchPageState();
-}
-
-class _StopWatchPageState extends State<StopWatchPage> {
-  late Timer _timer; // 타이머
-
-  var _time=0; // 0.01초마다 1씩 증가시킬 정수형 변수
-  var _isRunning =false; // 현재 시작 상태를 나타낼 불리언 변수
-
-  List<String> _lapTimes=[]; // 랩타임에 표시할 시간을 저장할 리스트
-
-  @override
-  void dispose(){
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StopWatch'),
-      ),
-      body: _buildBody(),
-      bottomNavigationBar: BottomAppBar( // 하단에 배치하는 AppBar
-        child: Container(
-          height: 50,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton( // 시작 버튼이 위치함
-        onPressed: ()=>setState(() {
-          _clickButton();
-        }),
-        child: Icon(Icons.play_arrow),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
-  }
-
-  // 내용 부분
-  Widget _buildBody(){
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 30),
-        child: Stack(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Row( // 시간을 표시하는 영역
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Text('0',
-                      style:TextStyle(fontSize: 50),
-                    ),
-                    Text('00'),
-                  ],
-                ),
-                Container( // 랩타임을 표시하는 영역
-                  width: 100,
-                  height: 200,
-                  child: ListView(
-                    children: <Widget>[
-                    ],
-                  ),
-                )
-              ],
-            ),
-            Positioned(
-              left: 10,
-              bottom: 10,
-              child: FloatingActionButton( // 왼쪽 아래에 위치한 초기화 버튼
-                backgroundColor: Colors.deepOrange,
-                onPressed: (){},
-                child: Icon(Icons.rotate_left),
-              ),
-            ),
-            Positioned(
-              right: 10,
-              bottom: 10,
-              child: ElevatedButton( // 오른쪽 아래에 위치한 랩타임 버튼
-                onPressed: (){},
-                child: Text('랩타임'),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-
-// 시작 또는 일시정지 버튼 클릭
-  void _clickButton(){
-    _isRunning=!_isRunning;
-
-    if(_isRunning){
-      _start();
-    }
-    else{
-      _pause();
-    }
-  }
-
-}
+// import 'package:flutter/material.dart';
+// import 'dart:async';
+//
+// void main() => runApp(MyApp());
+//
+// // 스탑워치
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'StopWatch',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: StopWatchPage(),
+//     );
+//   }
+// }
+//
+// class StopWatchPage extends StatefulWidget {
+//   const StopWatchPage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<StopWatchPage> createState() => _StopWatchPageState();
+// }
+//
+// class _StopWatchPageState extends State<StopWatchPage> {
+//   late Timer _timer; // 타이머
+//
+//   var _time=0; // 0.01초마다 1씩 증가시킬 정수형 변수
+//   var _isRunning =false; // 현재 시작 상태를 나타낼 불리언 변수
+//
+//   List<String> _lapTimes=[]; // 랩타임에 표시할 시간을 저장할 리스트
+//
+//   @override
+//   void dispose(){
+//     _timer?.cancel();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('StopWatch'),
+//       ),
+//       body: _buildBody(),
+//       bottomNavigationBar: BottomAppBar( // 하단에 배치하는 AppBar
+//         child: Container(
+//           height: 50,
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton( // 시작 버튼이 위치함
+//         onPressed: ()=>setState(() {
+//           _clickButton();
+//         }),
+//         child: Icon(Icons.play_arrow),
+//       ),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+//     );
+//   }
+//
+//   // 내용 부분
+//   Widget _buildBody(){
+//     return Center(
+//       child: Padding(
+//         padding: const EdgeInsets.only(top: 30),
+//         child: Stack(
+//           children: <Widget>[
+//             Column(
+//               children: <Widget>[
+//                 Row( // 시간을 표시하는 영역
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   crossAxisAlignment: CrossAxisAlignment.end,
+//                   children: <Widget>[
+//                     Text('0',
+//                       style:TextStyle(fontSize: 50),
+//                     ),
+//                     Text('00'),
+//                   ],
+//                 ),
+//                 Container( // 랩타임을 표시하는 영역
+//                   width: 100,
+//                   height: 200,
+//                   child: ListView(
+//                     children: <Widget>[
+//                     ],
+//                   ),
+//                 )
+//               ],
+//             ),
+//             Positioned(
+//               left: 10,
+//               bottom: 10,
+//               child: FloatingActionButton( // 왼쪽 아래에 위치한 초기화 버튼
+//                 backgroundColor: Colors.deepOrange,
+//                 onPressed: (){},
+//                 child: Icon(Icons.rotate_left),
+//               ),
+//             ),
+//             Positioned(
+//               right: 10,
+//               bottom: 10,
+//               child: ElevatedButton( // 오른쪽 아래에 위치한 랩타임 버튼
+//                 onPressed: (){},
+//                 child: Text('랩타임'),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//
+// // 시작 또는 일시정지 버튼 클릭
+//   void _clickButton(){
+//     _isRunning=!_isRunning;
+//
+//     if(_isRunning){
+//       _start();
+//     }
+//     else{
+//       _pause();
+//     }
+//   }
+//
+// }
