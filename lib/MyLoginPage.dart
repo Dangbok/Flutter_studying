53d4b6f44be09d941f69014565ea2f5d6ee11472
Dangbok <ghdwldnjs99@naver.com>
2022-11-17 13:47:44 +0900

    .ver3

--- lib/MyLoginPage.dart
+++ lib/MyLoginPage.dart
@@ -15,58 +15,59 @@ class MyLoginPage extends StatefulWidget {
 }
 
 class _MyLoginPageState extends State<MyLoginPage> {
-  // LoginPlatform _loginPlatform = LoginPlatform.none;
-
-  Future<OAuthToken> loginWithKakaoAccount({
-    List<Prompt>? prompts,
-    List<String>? channelPublicIds,
-    List<String>? serviceTerms,
-    String? loginHint,
-    String? nonce,
-  }) async {
-    String codeVerifier = AuthCodeClient.codeVerifier();
-    final authCode = await AuthCodeClient.instance.request(
-      prompts: prompts,
-      channelPublicIds: channelPublicIds,
-      serviceTerms: serviceTerms,
-      codeVerifier: codeVerifier,
-      loginHint: loginHint,
-      nonce: nonce,
-    );
-    final token = await AuthApi.instance
-        .issueAccessToken(authCode: authCode, codeVerifier: codeVerifier);
-    await TokenManagerProvider.instance.manager.setToken(token);
-    return token;
-  }
+  LoginPlatform _loginPlatform = LoginPlatform.none;
 
 
-  // void signInWithKakao() async {
-  //   try {
-  //     bool isInstalled = await isKakaoTalkInstalled();
-  //
-  //     OAuthToken token = isInstalled
-  //         ? await UserApi.instance.loginWithKakaoTalk()
-  //         : await UserApi.instance.loginWithKakaoAccount();
-  //
-  //     final url = Uri.https('kapi.kakao.com', '/v2/user/me');
-  //
-  //     final response = await http.get(
-  //       url,
-  //       headers: {
-  //         HttpHeaders.authorizationHeader: 'Bearer ${token.accessToken}'
-  //       },
-  //     );
-  //
-  //     final profileInfo = json.decode(response.body);
-  //     print(profileInfo.toString());
-  //
-  //     setState(() {
-  //       _loginPlatform = LoginPlatform.kakao;
-  //     });
-  //   } catch (error) {
-  //     print('카카오톡으로 로그인 실패 $error');
-  //   }
+  // Future<OAuthToken> loginWithKakaoAccount({
+  //   List<Prompt>? prompts,
+  //   List<String>? channelPublicIds,
+  //   List<String>? serviceTerms,
+  //   String? loginHint,
+  //   String? nonce,
+  // }) async {
+  //   String codeVerifier = AuthCodeClient.codeVerifier();
+  //   final authCode = await AuthCodeClient.instance.request(
+  //     prompts: prompts,
+  //     channelPublicIds: channelPublicIds,
+  //     serviceTerms: serviceTerms,
+  //     codeVerifier: codeVerifier,
+  //     loginHint: loginHint,
+  //     nonce: nonce,
+  //   );
+  //   final token = await AuthApi.instance
+  //       .issueAccessToken(authCode: authCode, codeVerifier: codeVerifier);
+  //   await TokenManagerProvider.instance.manager.setToken(token);
+  //   return token;
   // }
+
+
+  void signInWithKakao() async {
+    try {
+      bool isInstalled = await isKakaoTalkInstalled();
+
+      OAuthToken token = isInstalled
+          ? await UserApi.instance.loginWithKakaoTalk()
+          : await UserApi.instance.loginWithKakaoAccount();
+
+      final url = Uri.https('kapi.kakao.com', '/v2/user/me');
+
+      final response = await http.get(
+        url,
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer ${token.accessToken}'
+        },
+      );
+
+      final profileInfo = json.decode(response.body);
+      print(profileInfo.toString());
+
+      setState(() {
+        _loginPlatform = LoginPlatform.kakao;
+      });
+    } catch (error) {
+      print('카카오톡으로 로그인 실패 $error');
+    }
+  }
 
 
   // void _get_user_info() async {
@@ -80,19 +81,19 @@ class _MyLoginPageState extends State<MyLoginPage> {
   //   }
   // }
 
-  // void signOut() async {
-  //   switch (_loginPlatform) {
-  //     case LoginPlatform.kakao:
-  //       await UserApi.instance.logout();
-  //       break;
-  //     case LoginPlatform.none:
-  //       break;
-  //   }
-  //
-  //   setState(() {
-  //     _loginPlatform = LoginPlatform.none;
-  //   });
-  // }
+  void signOut() async {
+    switch (_loginPlatform) {
+      case LoginPlatform.kakao:
+        await UserApi.instance.logout();
+        break;
+      case LoginPlatform.none:
+        break;
+    }
+
+    setState(() {
+      _loginPlatform = LoginPlatform.none;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -104,31 +105,32 @@ class _MyLoginPageState extends State<MyLoginPage> {
         ),
         centerTitle: true,
       ),
+//       body: Center(
+//           child: ElevatedButton(
+//             child: const Text('kakaologin'),
+//             onPressed: () async {
+//               // print(await KakaoSdk.origin);
+//               loginWithKakaoAccount();
+//             },
+//           )),
+//     );
+//   }
+// }
+
       body: Center(
-          child: ElevatedButton(
-            child: const Text('kakaologin'),
-            onPressed: () {
-              loginWithKakaoAccount();
-            },
+          child: _loginPlatform != LoginPlatform.none
+              ? _logoutButton()
+              : Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _loginButton(
+                'kakao_logo',
+                signInWithKakao,
+              )
+            ],
           )),
     );
   }
-}
-
-  //     body: Center(
-  //         child: _loginPlatform != LoginPlatform.none
-  //             ? _logoutButton()
-  //             : Row(
-  //           mainAxisAlignment: MainAxisAlignment.center,
-  //           children: [
-  //             _loginButton(
-  //               'kakao_logo',
-  //               signInWithKakao,
-  //             )
-  //           ],
-  //         )),
-  //   );
-  // }
 
   // body: Container(
   //     color: Colors.white,
@@ -166,34 +168,34 @@ class _MyLoginPageState extends State<MyLoginPage> {
 //   );
 // }}
 
-//   Widget _loginButton(String path, VoidCallback onTap) {
-//     return Card(
-//       elevation: 5.0,
-//       shape: const CircleBorder(),
-//       clipBehavior: Clip.antiAlias,
-//       child: Ink.image(
-//         image: AssetImage('assets/$path.jpg'),
-//         width: 60,
-//         height: 60,
-//         child: InkWell(
-//           borderRadius: const BorderRadius.all(
-//             Radius.circular(35.0),
-//           ),
-//           onTap: onTap,
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget _logoutButton() {
-//     return ElevatedButton(
-//       onPressed: signOut,
-//       style: ButtonStyle(
-//         backgroundColor: MaterialStateProperty.all(
-//           const Color(0xff0165E1),
-//         ),
-//       ),
-//       child: const Text('로그아웃'),
-//     );
-//   }
-// }
+  Widget _loginButton(String path, VoidCallback onTap) {
+    return Card(
+      elevation: 5.0,
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      child: Ink.image(
+        image: AssetImage('assets/$path.jpg'),
+        width: 60,
+        height: 60,
+        child: InkWell(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(35.0),
+          ),
+          onTap: onTap,
+        ),
+      ),
+    );
+  }
+
+  Widget _logoutButton() {
+    return ElevatedButton(
+      onPressed: signOut,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+          const Color(0xff0165E1),
+        ),
+      ),
+      child: const Text('로그아웃'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() {
   // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
   WidgetsFlutterBinding.ensureInitialized();
   KakaoSdk.init(
-    nativeAppKey: '0f1772d6ba49dbd308c3df9873edf6e1',
-    javaScriptAppKey: '19ef6355b4d2cb8afc9c439c165794be',
+    nativeAppKey: '0f1772d6ba49dbd308c3df9873edf6e1'
+    // javaScriptAppKey: '19ef6355b4d2cb8afc9c439c165794be',
   );
   runApp(const MyApp());
 }
